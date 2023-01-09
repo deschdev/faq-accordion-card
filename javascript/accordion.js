55772fb2cb5728d6be27d4516cc2e4a746eda762
deschdev <94028614+deschdev@users.noreply.github.com>
2023-01-08 20:47:36 -0500

    Project update
    updated the project to not use javascript - removed divs and added an unordered list with list items. Inside of the list items there are now detail and summary elements.
    
    No JS accordion.

--- javascript/accordion.js
+++ javascript/accordion.js
@@ -1,16 +1,18 @@
-document.addEventListener("DOMContentLoaded", function() {
-  // getting the accordion element
-  const accordion = document.querySelector('.accordion-container');
-  const accordionQuestionHeader = accordion.querySelectorAll('.accordion-question');
-  // Get all of the headers that trigger the accordion
-  accordionQuestionHeader.forEach((question) => {
-    // Loop through all of the accordionQuestionHeaders and adds a click event listener to each
-    question.addEventListener("click", function() {
-      // get the dropdownPanel that the button controls
-      let dropdownPanel = question.nextElementSibling;
-      // Toggle the "active" class on the question and dropdownPanel to show / hide the answer
-      dropdownPanel.classList.toggle("active");
-      question.classList.toggle("active")
-    });
-  });
-});
+// wanted to use the same functionality but only with HTML and CSS 
+
+// document.addEventListener("DOMContentLoaded", function() {
+//   // getting the accordion element
+//   const accordion = document.querySelector('.accordion-container');
+//   const accordionQuestionHeader = accordion.querySelectorAll('.accordion-question');
+//   // Get all of the headers that trigger the accordion
+//   accordionQuestionHeader.forEach((question) => {
+//     // Loop through all of the accordionQuestionHeaders and adds a click event listener to each
+//     question.addEventListener("click", function() {
+//       // get the dropdownPanel that the button controls
+//       let dropdownPanel = question.nextElementSibling;
+//       // Toggle the "active" class on the question and dropdownPanel to show / hide the answer
+//       dropdownPanel.classList.toggle("active");
+//       question.classList.toggle("active")
+//     });
+//   });
+// });
